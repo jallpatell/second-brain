79735e9842794a952bf5f992df8f2bab057e7dd5
Jal <11jal.edu@gmail.com>
2024-12-27 23:12:47 +0530

    add signup endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import  express from 'express'
 import jwt from 'jsonwebtoken'
-
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
 const app = express()
+app.use(express.json())
 
 app.post(' /api/v1/signup', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    UserModel.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "user signed up"
+    })
 
 })
 
