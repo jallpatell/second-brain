51a08424c5c6946244a43336f7e597554db50bb9
Jal <11jal.edu@gmail.com>
2024-12-27 23:23:59 +0530

    update database-lvl1

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,25 @@
 // create user schemas and models 
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
 
-import { Model, Schema } from 'mongoose'
+mongoose.connect("mongodb://localhost:27017/ ")
 
 const UserSchema = new Schema({
-    username: { type: String, unique: true},
-    password: String
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // Add other fields as needed
+});
 
-export const UserModel = model(UserSchema "user")
+export const UserModel = model("User", UserSchema);
