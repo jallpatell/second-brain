1361c9f3948765e8426b5c33082632b5ed6ac4a5
Jal <11jal.edu@gmail.com>
2024-12-27 23:02:22 +0530

    create user schemas and models

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,10 @@
 // create user schemas and models 
+
+import { Model, Schema } from 'mongoose'
+
+const UserSchema = new Schema({
+    username: { type: String, unique: true},
+    password: String
+})
+
+export const UserModel = model(UserSchema "user")
